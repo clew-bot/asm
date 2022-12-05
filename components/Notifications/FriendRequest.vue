6afb44a67257a2b63932fc19eea8d3422c2f23e2
Chad Lew <lewchad11@gmail.com>
2022-12-05 10:21:12 -0800

    Adding in more stuff

--- components/Notifications/FriendRequest.vue
+++ components/Notifications/FriendRequest.vue
@@ -1,44 +1,62 @@
 <template>
     <v-card
-      class="mx-auto"
-
+      class="mx-auto bg-inherit"
+      elevation="0"
     >
       <v-list
         v-for="notification in modelValue"
         :key="notification"
-        lines="three"
-        :item="modelValue"
+        class="h-[7rem] border-4 border-slate-600 rounded-2xl bg-slate-700
+        transition-all"
+        
       >
       <v-card-title>
-          <div class="flex justify-items-start pt-3">
-
+          <div class="flex justify-items-start">
+            <NuxtLink
+                  clas="w-fit"
+                  :to="`/profile/${notification.from.handleName}`">
             <StatusUserAvatar class="pb-10" :props="notification.from.profilePicture"/>
-            <div class="absolute left-16 top-6 ml-2 w-fit">
-              <NuxtLink :to="`/profile/${notification.from.handleName}`">
+            </NuxtLink>
+            <div class="absolute left-16 top-3 ml-2 w-fit">
+          
                 <div
-                  class="text-lg font-semibold text-white hover:text-blue-300 cursor-pointer"
+                  class="text-base font-semibold text-white cursor-default"
                 >
                   {{notification.title}}
-                  <div>
-                    {{ notification.from.username }}
-                  </div>
-     
                 </div>
-              </NuxtLink>
+                <div class="text-sm text-amber-300 cursor-default">
+                  {{notification.content}}!
+                  </div>
+                  <NuxtLink
+                  :to="`/profile/${notification.from.handleName}`">
+                  <div class="text-xs font-bold hover:text-blue-300 hover:underline">
+                    {{notification.from.username}}
+                  </div>
+                </NuxtLink>
               <div class="text-xs text-gray-400 cursor-pointer">
-                <!-- @{{ notification.from.handleName }} -->
+                @{{ notification.from.handleName }}
               </div>
             </div>
 
             <div
-              class="text-slate-300 absolute top-5 right-6 text-xs cursor-default"
+              class="text-slate-300 absolute top-4 right-6 text-xs cursor-default"
             >
               {{ regularDate(notification.createdAt) }}
             </div>
             <div
-              class="text-slate-500 absolute top-9 right-6 text-xs cursor-default"
+              class="text-slate-500 absolute top-8 right-6 text-xs cursor-default"
             >
               {{ createdAtLog(notification.createdAt) }}
+            </div>
+            <div
+              class="text-slate-500 absolute bottom-2 right-10 text-xs cursor-default"
+            >
+              <IconComponent :props="{name: 'mdi-check-bold', size: 'large', color: 'green'}" />
+            </div>
+            <div
+              class="text-slate-500 absolute bottom-2 right-4 text-xs cursor-default"
+            >
+              <IconComponent :props="{name: 'mdi-close-thick', size: 'large', color: 'red'}" />
             </div>
           </div>
         </v-card-title>
--- components/Profile/ProfileHeader.vue
+++ components/Profile/ProfileHeader.vue
@@ -2,13 +2,29 @@
 
 </style>
 <template>
+  <div class="relative">
  <v-parallax
+    :class="heightChange ? 'max-h-[20rem]' : 'max-h-[100rem]'"
+    class="transition-all duration-500"
     :scale=".7"
     :src="props ? props : 'https://d33wubrfki0l68.cloudfront.net/8d79b89491cf9ef958be1386776dc937d39266be/1a630/img/particle_background.jpg'"
   ></v-parallax>
+  <IconComponent
+    :props="{name: 'mdi-chevron-down', size: 'default', color: 'white'}"
+    class="absolute bottom-1 left-1/2 transform -translate-x-1/2 bg-zinc-600 rounded-full"
+    :class="heightChange ? '' : 'rotate-180'"
+    @click="heightChange = !heightChange"/>
+  </div>
 </template>
 
 <script setup>
+
+
 const {props} = defineProps(['props']);
 console.log(props)
+const heightChange = ref(false)
+
+const test = () => {
+  heightChange.value = !heightChange.value
+}
 </script>
