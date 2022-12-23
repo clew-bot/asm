61e1270d78ae08a908cbac745c522100f3624beb
Chad Lew <lewchad11@gmail.com>
2022-12-22 22:26:19 -0800

    More work on message api

--- server/api/messages/send-message.post.ts
+++ server/api/messages/send-message.post.ts
@@ -9,7 +9,30 @@ export default defineEventHandler(async (event) => {
     const id:any = await useStorage().getItem("user");
     const myId = new toId(id);
     const body = await readBody(event);
-    console.log('body: ', body)
+    const {message, to} = body;
+    const toUserId = new toId(to);
+
+    const messageContent = await new MessageContentModel({
+        content: message,
+        from: myId,
+        to: toUserId
+    }).save();
+
+    const appendMessage = await UserModel.findOneAndUpdate(
+        {_id: myId},
+        // only push to messages if the message is not already in the array
+        {$addToSet: {messages: toUserId}},
+        {new: true}
+    );
+
+    const appendMessageTo = await UserModel.findOneAndUpdate(
+        {_id: toUserId},
+        // only push to messages if the message is not already in the array
+        {$addToSet: {messages: myId}},
+        {new: true}
+    );
+
+    console.log('body: ', messageContent)
     return "hi"
 
     
