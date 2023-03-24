0981448b9bc16c50b8830d544addc2f9978e2101
Chad Lew <lewchad11@gmail.com>
2023-03-24 13:30:26 -0700

    Fixing /single-posts population referencing correct models

--- server/api/dashboard/single-post.post.ts
+++ server/api/dashboard/single-post.post.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 import PostComment from "~~/server/models/PostComment.model";
 import UserPost from "~~/server/models/UserPost.model";
-
+import User from "~~/server/models/User.model";
+import PollModel from "~~/server/models/PollModel.model";
 const toId = mongoose.Types.ObjectId;
 
 export default defineEventHandler(async (event) => {
@@ -11,8 +12,19 @@ export default defineEventHandler(async (event) => {
         console.log(body)
    const findPost = await UserPost.findById({
          _id: body,
-        }).populate('author', ['username', 'handleName', 'profilePicture']).populate('comments').populate('poll').populate('comments.author', ['username', 'handleName', 'profilePicture']).sort({createdAt: -1}).exec();
-console.log(findPost)
+        })
+        // .populate('author', ['username', 'handleName', 'profilePicture'])
+        .populate({ path: 'author', model: User, select: ['username', 'handleName', 'profilePicture'] })
+        // .populate('comments')
+        .populate({ path: 'comments', model: PostComment, populate: { path: 'author', model: User, select: ['username', 'handleName', 'profilePicture'] } })
+        // .populate('poll')
+        .populate({ path: 'poll', model: PollModel })
+        // .populate('comments.author', ['username', 'handleName', 'profilePicture']).sort({createdAt: -1}).exec();
+        .populate({ path: 'comments.author', model: User, select: ['username', 'handleName', 'profilePicture'] }).sort({createdAt: -1}).exec();
+
+
+
+
     return findPost;
     } catch (error) {
         console.log(error)
